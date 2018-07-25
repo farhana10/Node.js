115d562dece9951749b09820db513bc63874fcfc
Farhana Hossain <farhana9552@yahoo.com>
2018-07-25 04:52:57 +0000

    classwork08

--- server.js
+++ server.js
@@ -5,6 +5,36 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({expected:true}));
 
+var mongo = require("mongodb");
+
+//for local
+//var db,uri = "mongod://localhost:27071";
+
+//for c9
+var db,uri = "mongodb://"+process.env.IP+":27017";
+
+
+mongo.MongoClient.connect(uri,{userNewUrlParser:true},function (err,clint )
+{if(err)
+{
+  console.log("Could not connect to mongo db")
+}else {
+  db=clint.db("node-cw8");
+}
+
+})
+
+var save = function(form_data)
+{
+  db.createCollection("users",function (err,collection){
+    
+  });
+  var collection = db.collection('users');
+  collection.save(form_data);
+}
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
 var server = http.Server(app);
 
 app.get('/', function(req,res){
@@ -21,6 +51,8 @@ app.get('/form', function(req,res){
 });
 app.post('/submit_user', function(req,res){
  console.log(req.body);
+ save(req.body);
+ res.status(200);
   
 });
   server.listen(process.env.PORT, process.env.IP, function(){
